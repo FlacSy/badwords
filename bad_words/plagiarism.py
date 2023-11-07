605e10f4d7e4830fae6103016e195aa4eeeb424b
FlacSy <125910405+FlacSy@users.noreply.github.com>
2023-11-07 10:52:45 +0300

    Update plagiarism.py
    Added typing for code

--- bad_words/plagiarism.py
+++ bad_words/plagiarism.py
@@ -1,23 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 from googlesearch import search
+from typing import List
 
 class PlagiarismChecker:
-    def __init__(self, query, page=5, text_to_check='', print_links=False):
+    def __init__(self, query: str, page: int = 5, text_to_check: str = '', print_links: bool = False) -> None:
         # Initialize the PlagiarismChecker with query, number of pages to search, text to check for plagiarism, and printing option
         self.query = query
         self.page = page
         self.text_to_check = text_to_check
         self.print_links = print_links
 
-    def url_list(self):
+    def url_list(self) -> List[str]:
         # Generate a list of URLs from a Google search query, limited to a specified number of pages
         urls = []
         for url in search(self.query, num_results=10):
             urls.append(url)
         return urls[:int(self.page)]
 
-    def check_website_for_plagiarism(self, website_url):
+    def check_website_for_plagiarism(self, website_url: str) -> bool:
         try:
             # Send an HTTP GET request to the website and parse the HTML content
             response = requests.get(website_url)
@@ -31,7 +32,7 @@ class PlagiarismChecker:
 
         return False
 
-    def check_plagiarism(self):
+    def check_plagiarism(self) -> bool:
         # Iterate through the list of URLs and check each one for plagiarism
         for i, link in enumerate(self.url_list(), start=1):
             if self.print_links:
