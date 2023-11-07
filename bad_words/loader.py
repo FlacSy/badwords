6415e33ee2e86a351fa94231cea0d9f058c28d62
FlacSy <125910405+FlacSy@users.noreply.github.com>
2023-11-07 10:51:41 +0300

    Update loader.py
    Added typing for code

--- bad_words/loader.py
+++ bad_words/loader.py
@@ -2,15 +2,15 @@ import os
 import shutil
 
 class Load:
-    def __init__(self):
+    def __init__(self) -> None:
         # Get the path to the directory of the current script
-        self.script_dir = os.path.dirname(os.path.realpath(__file__))
+        self.script_dir = os.path.dirname(os.path.realpath(__file__)
         
         # Define directories for bad words and good words resources
         self.resource_dir_bad_words = os.path.join(self.script_dir, 'resource/bad_words')
         self.resource_dir_good_words = os.path.join(self.script_dir, 'resource/good_words')
 
-    def update_file(self, source_file, target_dir):
+    def update_file(self, source_file: str, target_dir: str) -> None:
         # Check if the source file exists and is a file
         if os.path.exists(source_file) and os.path.isfile(source_file):
             # Generate the target file path by joining the target directory and the source file's base name
@@ -25,16 +25,16 @@ class Load:
         else:
             raise FileNotFoundError(f"File not found at {source_file}")
 
-    def load_bad_file(self, file_path):
+    def load_bad_file(self, file_path: str) -> None:
         # Load the specified file into the bad words resource directory
         self.update_file(file_path, self.resource_dir_bad_words)
 
-    def load_good_file(self, file_path):
+    def load_good_file(self, file_path: str) -> None:
         # Load the specified file into the good words resource directory
         self.update_file(file_path, self.resource_dir_good_words)
 
 class Unload:
-    def __init__(self):
+    def __init__(self) -> None:
         # Get the path to the directory of the current script
         self.script_dir = os.path.dirname(os.path.realpath(__file__))
         
@@ -42,7 +42,7 @@ class Unload:
         self.resource_dir_bad_words = os.path.join(self.script_dir, 'resource/bad_words')
         self.resource_dir_good_words = os.path.join(self.script_dir, 'resource/good_words')
 
-    def unload_file(self, source_file, target_dir):
+    def unload_file(self, source_file: str, target_dir: str) -> None:
         # Check if the source file exists and is a file
         if os.path.exists(source_file) and os.path.isfile(source_file):
             # Generate the target file path by joining the target directory and the source file's base name
@@ -57,10 +57,10 @@ class Unload:
         else:
             raise FileNotFoundError(f"File not found at {source_file}")
 
-    def unload_bad_file(self, file_path):
+    def unload_bad_file(self, file_path: str) -> None:
         # Unload the specified file from the bad words resource directory
         self.unload_file(file_path, self.resource_dir_bad_words)
 
-    def unload_good_file(self, file_path):
+    def unload_good_file(self, file_path: str) -> None:
         # Unload the specified file from the good words resource directory
         self.unload_file(file_path, self.resource_dir_good_words)
