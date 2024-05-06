d5e212ba6dd0a90dee5f62d8f873f035115ef3cd
Андрей <125910405+FlacSy@users.noreply.github.com>
2024-05-07 00:42:40 +0200

    Update check.py

--- badwords/check.py
+++ badwords/check.py
@@ -9,20 +9,18 @@ class Check:
         
         # Initialize a dictionary containing paths to files with bad words for different languages
         self.language_files: Dict[str, str] = self.initialize_language_files()
-
-        # If no languages are specified, use 'ru' (Russian) as the default
-        if not languages:
-            languages = ['ru']
-
+        self.languages = languages
         # If 'all_languages' is set to True, use all available languages
         if all_languages:
-            languages = list(self.language_files.keys())
+            self.languages = list(self.language_files.keys())
 
         # Initialize a dictionary of bad words for the specified languages
         self.bad_words: Dict[str, List[str]] = self.initialize_bad_words(languages)
 
         # Compile regular expressions for matching bad words
         self.patterns: Dict[str, re.Pattern] = self.compile_patterns()
+
+        
 
     def initialize_language_files(self) -> Dict[str, str]:
         language_files = {}
@@ -33,31 +31,32 @@ class Check:
             language_code = os.path.splitext(filename)[0]
             file_path = os.path.join(resource_dir, filename)
             language_files[language_code] = file_path
+
         return language_files
 
     def initialize_bad_words(self, languages: List[str]) -> Dict[str, List[str]]:
         bad_words = {}
-        for language in languages:
-            file_path = self.language_files.get(f"{language}.bdw")
+        for language in self.languages:
+            file_path = self.language_files.get(language)
             if file_path is not None:
                 with open(file_path, 'r', encoding='utf-8') as file:
                     # Store bad words as a list of strings, removing leading/trailing whitespaces
                     bad_words[language] = [line.strip() for line in file]
         return bad_words
 
-    def compile_patterns(self, case_insensitive: bool = True) -> Dict[str, re.Pattern]:
+    def compile_patterns(self) -> Dict[str, re.Pattern]:
         patterns = {}
-        flags = re.IGNORECASE if case_insensitive else 0
         for language, words in self.bad_words.items():
-            patterns[language] = re.compile(r'(?:^|\s)(?:' + '|'.join(re.escape(word) for word in words) + r')(?=\s|$)', flags)
+            patterns[language] = re.compile(r'\b(?:' + '|'.join(re.escape(word) for word in words) + r')\b', re.IGNORECASE)
         return patterns
 
-    def filter_profanity(self, text: str, language: str = 'ru') -> bool:
-        if language not in self.language_files:
-            raise ValueError(f'Unsupported language: {language}')
-        if language not in self.patterns:
-            return False
-
-        if self.patterns[language].search(text):
-            return True
+    def filter_profanity(self, text: str) -> bool:
+        for language in self.languages:
+            if language not in self.language_files:
+                raise ValueError(f'Unsupported language: {language}')
+            if language not in self.patterns:
+                return False
+            
+            if self.patterns[language].search(text):
+                return True
         return False
