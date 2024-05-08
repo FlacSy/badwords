13676644886b2a1914e11cf2fd3c763f0815a5a9
FlacSy <flacsy.x@gmail.com>
2024-05-08 11:26:34 +0200

    ~

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# from badwords.anailzator import SentimentAnalyzer
+from badwords.check import Checker
+from badwords.delete import Delete
+from badwords.replace import Replace
+from badwords.keyword import KeywordExtractor
+from badwords.loader import Load, Unload
--- badwords/__init__.py
+++
@@ -1,6 +0,0 @@
-# from .anailzator import SentimentAnalyzer
-from .check import Checker
-from .delete import Delete
-from .replace import Replace
-from .keyword import KeywordExtractor
-from .loader import Load, Unload
