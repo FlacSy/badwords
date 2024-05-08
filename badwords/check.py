3037caa99c5230a4497f65165951154c350e3856
Андрей <125910405+FlacSy@users.noreply.github.com>
2024-05-08 11:36:59 +0200

    Update check.py

--- badwords/check.py
+++ badwords/check.py
@@ -4,43 +4,30 @@ from typing import List, Dict
 
 class Check:
     def __init__(self, languages: List[str] = None, all_languages: bool = False):
-        # Get the path to the current script
         self.script_dir = os.path.dirname(os.path.realpath(__file__))
-        
-        # Initialize a dictionary containing paths to files with bad words for different languages
         self.language_files: Dict[str, str] = self.initialize_language_files()
         self.languages = languages
-        # If 'all_languages' is set to True, use all available languages
         if all_languages:
             self.languages = list(self.language_files.keys())
-
-        # Initialize a dictionary of bad words for the specified languages
-        self.bad_words: Dict[str, List[str]] = self.initialize_bad_words(languages)
-
-        # Compile regular expressions for matching bad words
+        self.bad_words: Dict[str, List[str]] = self.initialize_bad_words(self.languages)
         self.patterns: Dict[str, re.Pattern] = self.compile_patterns()
-
         self.custom_bad_words: List[str] = []
 
     def initialize_language_files(self) -> Dict[str, str]:
         language_files = {}
         resource_dir = os.path.join(self.script_dir, 'resource\\bad_words')
-
-        # Get a list of files containing bad words for different languages
         for filename in os.listdir(resource_dir):
             language_code = os.path.splitext(filename)[0]
             file_path = os.path.join(resource_dir, filename)
             language_files[language_code] = file_path
-
         return language_files
 
     def initialize_bad_words(self, languages: List[str]) -> Dict[str, List[str]]:
         bad_words = {}
-        for language in self.languages:
+        for language in languages:
             file_path = self.language_files.get(language)
             if file_path is not None:
                 with open(file_path, 'r', encoding='utf-8') as file:
-                    # Store bad words as a list of strings, removing leading/trailing whitespaces
                     bad_words[language] = [line.strip() for line in file]
         return bad_words
 
@@ -54,16 +41,10 @@ class Check:
         self.custom_bad_words.extend(words)
 
     def filter_profanity(self, text: str) -> bool:
-        all_bad_words = self.custom_bad_words
+        all_bad_words = set(self.custom_bad_words)
         for language in self.languages:
-            if language not in self.language_files:
-                raise ValueError(f'Unsupported language: {language}')
-            if language not in self.patterns:
-                raise ValueError(f'Patterns not compiled for language: {language}')
-            
-            all_bad_words.extend(self.bad_words.get(language, []))
-
-        for bad_word in all_bad_words:
-            if bad_word.lower() in text.lower():
-                return True
-        return False
+            all_bad_words.update(self.bad_words.get(language, []))
+        
+        pattern = re.compile(r'\b(?:' + '|'.join(map(re.escape, all_bad_words)) + r')\b', re.IGNORECASE)
+        matches = pattern.findall(text)
+        return bool(matches)
